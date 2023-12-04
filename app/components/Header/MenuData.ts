072c8cc4d3fdbcf4d29072a17081ce4c55f7ca1e
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-04 08:37:37 +0300

    Chore:Created the Navbar

---
+++ app/components/Header/MenuData.ts
@@ -0,0 +1,34 @@
+const menuData = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Events",
+    path: "/events",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "News",
+    path: "/news",
+    newTab: false,
+  },
+
+  {
+    id: 5,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 6,
+    title: "Sponsorship",
+    path: "/sponsorship",
+    newTab: false,
+  },
+];
+export default menuData;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
@@ -63,7 +63,7 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
-    require("flowbite/plugin"),
+
     // ...
   ],
 };
