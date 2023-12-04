bc716bdbd9b03c811dc29140d70de5bc6c2f6bcc
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-04 06:11:11 +0300

    Chore:Create  theme-provider.tsx and theme-toggler button to change between light and dark mode

---
+++ app/components/Header/ThemeToggler.tsx
@@ -0,0 +1,29 @@
+"use client ";
+
+import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { RiSunLine, RiMoonClearLine } from "react-icons/ri";
+
+function ThemeToggler() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="flex items-center focus:outline-none"
+    >
+      {theme === "dark" ? (
+        <div className="mr-2 text-4xl text-[#4a576E]">
+          <RiSunLine />
+        </div>
+      ) : (
+        <div className="mr-2 text-4xl text-gray-700">
+          <span className="text-[#4a576E]">
+            <RiMoonClearLine />
+          </span>
+        </div>
+      )}
+    </button>
+  );
+}
+
+export default ThemeToggler;
