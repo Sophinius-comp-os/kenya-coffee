9dd9416d47d6fd5f1c1fb1b80c92a56be405ec14
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-06 05:08:47 +0300

    Chore:Added the events schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -4,7 +4,8 @@ import blockContent from './schemas/blockContent'
 import category from './schemas/category'
 import post from './schemas/post'
 import author from './schemas/author'
+import event from './schemas/event'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
+  types: [post, author, category, blockContent, event],
 }
