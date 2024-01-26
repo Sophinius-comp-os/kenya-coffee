1da6f2b98a3b5033a8fde13ad6eb9fc2ffb05cc6
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-01-26 18:36:33 +0300

    Made changes to the app to make the gallery page render

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,13 +7,13 @@ import { FeaturedPosts } from "@/components/blog/FeaturedPosts";
 import { fetchEvents, fetchPosts } from "@/sanity/sanity.query";
 import { Post } from "@/typings";
 import About from "@/components/About";
-import delay from 'delay'
+
 export const revalidate = 3600;
 
 const Home = async () => {
   const posts: [Post] = await fetchPosts();
   const events: [Post] = await fetchEvents();
-  await delay(5000)
+
 
   return (
     <div className="relative">
