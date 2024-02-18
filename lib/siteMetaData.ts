9e9cce473607edf3e4368874962abd2d725378dd
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-02-18 10:44:26 +0300

    Create siteMetaData.ts

---
+++ lib/siteMetaData.ts
@@ -0,0 +1,20 @@
+// please update required information
+const siteMetadata = {
+    title: 'Kenya Coffee Events',
+    author: 'mcwachira',
+    headerTitle: 'Next.js  website',
+    description: 'A website built using Nextjs and sanity .',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'www.kenyacoffeeevents.com', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'eventskenyacoffee@gmail.com',
+    github: 'https://github.com/mcwachira',
+    twitter: 'https://twitter.com/CoffeeEventsKE',
+    facebook: 'https://www.facebook.com/CoffeeEventsKE?mibextid=kFxxJD',
+    instagram:'https://www.instagram.com/coffeeeventske',
+    locale: 'en-US',
+}
+
+module.exports = siteMetadata
